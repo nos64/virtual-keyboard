6c7507d74f58b2c883cb69ccc38eef032c899481
Mikhail Nosov <manosov1984@gmail.com>
2022-05-08 06:00:03 +0400

    feat: finish render keyboard

--- src/script/index.js
+++ src/script/index.js
@@ -82,44 +82,52 @@ import {langKey} from './language.js';
 //   }
 // }
 
-let language = 'ru';
-// console.log((langKey[language]).find(item => item.code === 'Digit2'))
+let language = 'en';
+// console.log((langKey[language]).find(item => item.code === 'Digit9'))
 // console.log(Object.values(langKey[language]).forEach(item => console.log(item.code)))
-console.log(langKey[language][0].code)
+// console.log(langKey[language][0].code)
 
-const buttons = [
+const buttonsKbd = [
       ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
-      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete'],
       ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
       ['ShiftLeft', 'IntlBackslash', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
       ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
     ];
+const serviceBtn =  ['Backspace', 'Tab', 'CapsLock', 'ShiftLeft','ControlLeft', 'MetaLeft', 'AltLeft', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp', 'ShiftRight', 'Enter', 'Delete']
 
+const createBtn = ({symbol, shift, code}) => {
+      const button = document.createElement('div');
+      button.className = 'button';
+      button.dataset.code = code;
+      button.innerHTML = symbol;
+      if (serviceBtn.includes(button.dataset.code)) button.classList.add('service');
+      
+      const subButton = document.createElement('span');
+      subButton.className = 'sub-button';
+      subButton.innerHTML = shift;
+      
+      button.append(subButton);
 
-const createBtn = () => {
-      // const button = document.createElement('div');
-      // button.className = 'button';
-      // button.dataset.code = elem;
-      // const subButton = document.createElement('span');
-      // subButton.className = 'sub-button';
+      return button;
 }
 
 
-const createKeyboard = (language) => {
+const createKeyboard = () => {
 
   const keyboardWrapper = document.createElement('div');
   keyboardWrapper.className = 'keyboard-wrapper';
   const buttonsArray = [];
-  buttons.forEach(rows => {
+  buttonsKbd.forEach(rows => {
     const row = document.createElement('div');
     row.className = 'row';
 
     rows.forEach(btn => {
-      const btnSymbol = langKey[language].find(item => item.code === btn)
-      console.log('btnSymbol: ', btnSymbol);
-      // const button = createBtn(btnSymbol);
+      const btnObj = (langKey[language]).find(item => item.code === btn)
+      console.log('btnObj: ', btnObj);
+      const button = createBtn(btnObj);
 
-      // row.append(button);
+      row.append(button);
     })
 
     keyboardWrapper.append(row);
@@ -155,7 +163,7 @@ const createKeyboard = (language) => {
     subtitle.className = 'subtitle';
     subtitle.textContent = 'Press Shift+Ctrl to change language';
 
-    const keyboarFunc = createKeyboard(language)
+    const keyboarFunc = createKeyboard()
 
     containerMain.append(textarea, subtitle, keyboarFunc);
     main.append(containerMain);
